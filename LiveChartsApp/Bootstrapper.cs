070e4814ee1f4a585146ed7e86966def7b5ce454
Artur <arturdrm@hotmail.com>
2021-10-11 19:06:37 -0300

    ADDED: Bootstrapper

---
+++ LiveChartsApp/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using LiveChartsApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace LiveChartsApp
+{
+   public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ LiveChartsApp/ViewModels/ShellViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiveChartsApp.ViewModels
+{
+   public class ShellViewModel
+    {
+    }
+}
